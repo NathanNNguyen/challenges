458ab984489e58d790761faedd40c17ea24e75b2
Nhan <natenguyen51294@gmail.com>
2020-08-22 21:14:57 -0500

    non dup question

---
+++ interview_questions/non_dup.py
@@ -0,0 +1,31 @@
+# Given a list of numbers, where every number shows up twice except for one number, find that one number.
+
+# Example:
+# Input: [4, 3, 2, 4, 1, 3, 2]
+# Output: 1
+# Here's the function signature:
+
+
+# def singleNumber(nums):
+#   # Fill this in.
+
+
+# print singleNumber([4, 3, 2, 4, 1, 3, 2])
+# # 1
+
+def singleNumber(nums):
+    cache = {}
+    for i in nums:
+        if i not in cache:
+            cache[i] = 1
+        else:
+            cache[i] += 1
+    for item in cache.items():
+        if item[1] == 1:
+            result = item[0]
+            return result
+    return None
+
+
+arr = [4, 3, 2, 4, 1, 3, 2]
+print(singleNumber(arr))
