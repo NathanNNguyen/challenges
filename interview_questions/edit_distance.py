41da5648a1f94313a657a6544bbf05f4050d4705
Nhan <natenguyen51294@gmail.com>
2020-08-29 20:58:55 -0500

    first pass solution

--- interview_questions/edit_distance.py
+++ interview_questions/edit_distance.py
@@ -14,21 +14,52 @@ def distance(s1, s2):
     cache = {}
     len1 = len(s1)
     len2 = len(s2)
+    count = 0
+
+    # check for the length of both strings
+    # see how difference it is
     if len2 > len1:
-        for i in range(len2):
-            if s2[i] not in cache:
-                cache[s2[i]] = 1
+        # if len(s2) > len(s1):
+        count = len2 - len1
+        for i in s2:
+            if i not in cache:
+                cache[i] = 1
             else:
-                cache[s2[i]] += 1
-    for i in s1:
-        if i in cache:
-            if cache[i] >= 1:
-                print(i, cache[i])
-        else:
-            print(i, 'not in cache')
+                cache[i] += 1
+        for k, v in cache.items():
+            if k not in s1:
+                count += v
+
+    # if len(s1) > len(s2):
+    elif len1 > len2:
+        count = len1 - len2
+        for i in s1:
+            if i not in cache:
+                cache[i] = 1
+            else:
+                cache[i] += 1
+        for k, v in cache.items():
+            if k not in s2:
+                count += v
+
+    # if len(s2) == len(s1):
+    else:
+        for i in s1:
+            if i not in cache:
+                cache[i] = 1
+            else:
+                cache[i] += 1
+        for k, v in cache.items():
+            if k not in s2:
+                count += v
+
+    return count
 
 
 s1 = 'biting'
 s2 = 'sitting'
+
+s1 = 'kiting'
+s2 = 'biting'
 
 print(distance(s1, s2))
